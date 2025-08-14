a986352a7991e8ff50c8b248c4991ebb3f462497
Facundo Castillo <fcastillo@upostal.com.uy>
2025-08-14 16:27:33 -0300

    solucion-click

--- eventos.js
+++ eventos.js
@@ -4,3 +4,10 @@ let contenedor = document.getElementsByClassName('contenedor')[0];
 contenedor.addEventListener('click', function() {
     alert('Hola! Soy el div');
 });
+
+let boton = document.getElementById('btnSaludar');
+
+
+boton.addEventListener('click', function(event) {
+    event.stopPropagation();
+});
